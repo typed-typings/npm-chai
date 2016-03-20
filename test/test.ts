d0953f2ceb1d5d52ae81d410017f467e80548aff
Homa Wong <homawong@gmail.com>
2016-03-19 17:13:33 -0700

    Update tests

--- test/test.ts
+++ test/test.ts
@@ -1,16 +1,27 @@
 /// <reference path="./main.d.ts" />
+/// <reference path="../typings/main.d.ts" />
+import test = require('blue-tape');
+
 import c = require('chai');
 
-c.should()
-
-c.use((chai, utils) => {
-  utils.addChainableMethod(chai.Assertion.prototype, 'foo', function (str) {
-    let obj = utils.flag(this, 'object');
-    new chai.Assertion(obj).to.be.equal(str);
-  })
+test('use()', (t) => {
+  t.test('addChainableMethod()', (t) => {
+    t.plan(1);
+    c.use((chai, utils) => {
+      utils.addChainableMethod(chai.Assertion.prototype, 'foo', function(str) {
+        let obj = utils.flag(this, 'object');
+        new chai.Assertion(obj).to.be.equal(str);
+      });
+      t.assert(typeof (new chai.Assertion({})).foo === 'function', 'foo is function');
+      t.end();
+    });
+  });
 });
 
-let someObject = {};
-someObject.should.be.an('object');
-
-console.log('DONE');
+test('BDD', (t) => {
+  c.should();
+  let someObject = {};
+  someObject.should.be.an('object');
+  t.pass('should.be.an()');
+  t.end();
+});
