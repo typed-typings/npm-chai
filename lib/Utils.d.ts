a31a6634650911feaed5a4fa6c5739e05b6fc315
nicojs <jansennico@gmail.com>
2016-04-02 21:39:01 +0200

    Add return values for Utils

--- lib/Utils.d.ts
+++ lib/Utils.d.ts
@@ -1,20 +1,27 @@
 import {Assertion} from './Assertion';
 
+export interface PathInfo {
+  parent: any;
+  name: number|string;
+  value: any;
+  exists: boolean;
+}
+
 export interface Utils {
-  addChainableMethod(ctx: any, name: string, chainingBehavior: (value: any) => void);
-  addMethod(ctx: any, name: string, method: (value: any) => void);
-  addProperty(ctx: any, name: string, getter: () => void);
-  expectTypes(obj: Object, types: string[]);
-  flag(obj: Object, key: string, value?: any);
-  getActual(obj: Object, actual?: any);
-  getEnumerableProperties(obj: Object);
-  getMessage(obj: Object, params: any[]);
-  getMessage(obj: Object, message: string, negateMessage: string);
-  getName(func: Function);
-  getPathInfo(path: string, obj: Object);
-  getPathValue(path: string, obj: Object);
-  getProperties(obj: Object);
-  hasProperty(obj: Object, name: string);
-  transferFlags(assertion: Assertion | any, obj: Object, includeAll?: boolean);
-  inspect(obj: any);
+  addChainableMethod(ctx: any, name: string, chainingBehavior: (value: any) => void): void;
+  addMethod(ctx: any, name: string, method: (value: any) => void): void;
+  addProperty(ctx: any, name: string, getter: () => void): void;
+  expectTypes(obj: Object, types: string[]): void;
+  flag(obj: Object, key: string, value?: any): any;
+  getActual(obj: Object, actual?: any): any;
+  getEnumerableProperties(obj: Object): string[];
+  getMessage(obj: Object, params: any[]): string;
+  getMessage(obj: Object, message: string, negateMessage: string): string;
+  getName(func: Function): string;
+  getPathInfo(path: string, obj: Object): PathInfo;
+  getPathValue(path: string, obj: Object): any;
+  getProperties(obj: Object): string[];
+  hasProperty(obj: Object, name: string): boolean;
+  transferFlags(assertion: Assertion | any, obj: Object, includeAll?: boolean): void;
+  inspect(obj: any): any;
 }
